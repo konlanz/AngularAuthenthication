d89dc5ce3fc9b1d84830eefabb8529616b5de107
konlan mikpekoah <konlanmikpekoah.km@outlook.com>
2021-07-14 14:08:06 +0000

    voterdabase

---
+++ src/app/votedatabase.model.spec.ts
@@ -0,0 +1,7 @@
+import { Votedatabase } from './votedatabase.model';
+
+describe('Votedatabase', () => {
+  it('should create an instance', () => {
+    expect(new Votedatabase()).toBeTruthy();
+  });
+});
